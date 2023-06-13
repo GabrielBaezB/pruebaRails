d683c916c23660a24bdad843bf3d048109d68236
GabrielBaezB <gabooo@live.com>
2023-06-13 20:32:07 +0000

    se cargan imagnes en proyecto y se crean las rutas con el comando rails g c pages

--- Gemfile
+++ Gemfile
@@ -21,6 +21,10 @@ gem "importmap-rails"
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
 gem "turbo-rails"
 
+gem "bootstrap", "5.3.0.alpha3"
+
+gem "mini_racer"
+
 # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
 gem "stimulus-rails"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/projects'
-  get 'pages/contact'
+  get '/home', to: 'pages#home'
+  root 'pages#home'
+  get '/projects', to: 'pages#projects'
+  get '/contact', to: 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
